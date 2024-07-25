cf2352660a71fde8958389597e0143e6fd994a26
Quiatus <Quiatus@gmail.com>
2024-07-25 22:20:38 +0200

    add Pizza component

--- 02-pizza-menu/src/index.js
+++ 02-pizza-menu/src/index.js
@@ -1,8 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import './index.css'
 
 function App() {
-  return <div>
+  return <div className="container">
     <Header />
     <Menu />
     <Footer />
@@ -10,14 +11,40 @@ function App() {
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>
+  //const h1Style = { color: "red", fontSize: '48px', textTransform: 'uppercase' }
+  return <header className="header">
+    <h1 /* style={h1Style} */>Fast React Pizza Co.</h1>
+  </header>
 }
 
 function Menu() {
-  return <div>
+  return <main className="menu">
     <h2>Our menu</h2>
-    <Pizza />
-  </div>
+    <Pizza 
+      name="Pizza Spinaci" 
+      ingredients="Tomato, mozarella, spinach, and ricotta cheese" 
+      photoName="pizzas/spinaci.jpg" 
+      price={10}
+    />
+
+    <Pizza 
+      name="Pizza Funghi" 
+      ingredients="Tomato, mushrooms" 
+      photoName="pizzas/funghi.jpg" 
+      price={12}
+    />
+  </main>
+}
+
+function Pizza(props) {
+  return <div className="pizza">
+    <img src={props.photoName} alt={props.name}/>
+    <div>
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+      <span>{props.price}</span>
+    </div>
+  </div> 
 }
 
 function Footer() {
@@ -26,16 +53,7 @@ function Footer() {
   const closeHour = 22
   const isOpen = hour >= openHour && hour <= closeHour
   
-  
-  return <footer>{new Date().toLocaleDateString()} We're currently open</footer>
-}
-
-function Pizza() {
-  return <div>
-    <img src="pizzas/spinaci.jpg" alt="Pizza Spinazi"/>
-    <h2>Pizza</h2>
-    <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-  </div> 
+  return <footer>{new Date().toLocaleDateString()} {isOpen ? `We're currently open` : `Sorry, we're closed.`}</footer>
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
