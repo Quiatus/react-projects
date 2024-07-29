dec57b6dbc6f72b821e1931919e06245bbe260f8
Quiatus <Quiatus@gmail.com>
2024-07-29 22:08:58 +0200

    add style

--- 03-steps/src/App.js
+++ 03-steps/src/App.js
@@ -7,19 +7,27 @@ const messages = [
 export default function App() {
   const step = 1
 
+  function handlePrevious() {
+
+  }
+
+  function handleNext() {
+
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}>3</div>
       </div>
 
       <p className="message">Step {step}: {messages[step - 1]}</p>
 
       <div className="buttons">
-        <button style={{ backgroundColor: '#7950f2', color: '#FFF'}}>Previous</button>
-        <button style={{ backgroundColor: '#7950f2', color: '#FFF'}}>Next</button>
+        <button style={{ backgroundColor: '#7950f2', color: '#FFF'}} onClick={handlePrevious}>Previous</button>
+        <button style={{ backgroundColor: '#7950f2', color: '#FFF'}} onClick={handleNext}>Next</button>
       </div>
     </div>
   )
