b59ec85d4c400dd8ffbfd7ed786df5846d911203
Quiatus <Quiatus@gmail.com>
2024-08-05 19:47:17 +0200

    add stats feature

--- 04-travel-list/src/App.js
+++ 04-travel-list/src/App.js
@@ -26,7 +26,7 @@ export default function App() {
       <Logo /> 
       <Form onAddItems={handleAddItem}/> 
       <PackingList items={items} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem}/> 
-      <Stats /> 
+      <Stats items={items}/> 
     </div>
   )
 }
@@ -87,11 +87,22 @@ function Item({ item, onDeleteItem, onToggleItem }) {
   )
 }
 
-function Stats() {
+function Stats({items}) {
+  if (!items.length) 
+  return (
+    <p className='stats'>
+      <em>Start adding some items to your packing list 🚀</em> 
+    </p>
+  )
+
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length
+  const percentage = Math.round(numPacked / numItems * 100)
+
   return (
     <footer className="stats">
       <em>
-        You have X items on your list and you already packed X (X%)
+        {percentage === 100 ? 'You got everything! Ready to go ✈️' : `You have ${numItems} ${numItems === 1 ? 'item' : 'items'} on your list and you already packed ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   )
