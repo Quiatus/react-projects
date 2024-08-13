a854b56e402207bfe67ec7de5c3b88aeed853f13
Quiatus <Quiatus@gmail.com>
2024-08-13 15:53:34 +0200

    create star rating component

--- 06-usepopcorn/src/index.js
+++ 06-usepopcorn/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import StarRating from './StarRating';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']} defaultRating={3}/>
+    <StarRating maxRating={10} color='red'/>
   </React.StrictMode>
 );
