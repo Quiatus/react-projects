40b602eb2be975b80bf030e08d7a441f7ea73247
Quiatus <Quiatus@gmail.com>
2024-08-19 16:18:56 +0200

    add key prop

--- 05-eat-n-split/src/App.js
+++ 05-eat-n-split/src/App.js
@@ -54,7 +54,7 @@ export default function App() {
         <Button onClick={handleShowAddFriend}>{showAddFriend ? 'Close' : 'Add friend'}</Button>
       </div>
 
-      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill}/>}
+      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} onSplitBill={handleSplitBill} key={selectedFriend.id}/>}
     </div>
   );
 }
