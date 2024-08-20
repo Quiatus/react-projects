e7ccd808fa237451d4e17e57d90601d25ac72e3b
Quiatus <Quiatus@gmail.com>
2024-08-20 16:30:30 +0200

    add buffering

--- 07-how-react-works/src/App.js
+++ 07-how-react-works/src/App.js
@@ -62,8 +62,26 @@ function TabContent({ item }) {
   const [showDetails, setShowDetails] = useState(true);
   const [likes, setLikes] = useState(0);
 
+  console.log('RENDEER')
+
   function handleInc() {
-    setLikes(likes + 1);
+    setLikes(s => s + 1);
+  }
+
+  function handleTripleInc() {
+    setLikes(s => s + 1);
+    setLikes(s => s + 1);
+    setLikes(s => s + 1);
+  }
+
+  function handleUndo() {
+    setShowDetails(true)
+    setLikes(0)
+    console.log(likes)
+  }
+
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000)
   }
 
   return (
@@ -79,13 +97,13 @@ function TabContent({ item }) {
         <div className="hearts-counter">
           <span>{likes} ❤️</span>
           <button onClick={handleInc}>+</button>
-          <button>+++</button>
+          <button onClick={handleTripleInc}>+++</button>
         </div>
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndo}>Undo</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
