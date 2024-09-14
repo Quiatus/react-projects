9e72b29c0c8de2e17eeb54f5f74156207e5688dd
Quiatus <Quiatus@gmail.com>
2024-09-14 23:55:28 +0200

    add css modules

--- 09-worldwise/src/pages/Homepage.jsx
+++ 09-worldwise/src/pages/Homepage.jsx
@@ -1,12 +1,16 @@
 import PageNav from "../components/PageNav"
+import AppNav from "../components/AppNav"
+import { Link } from "react-router-dom"
 
 export default function Homepage() {
   return (
     <div>
 
       <PageNav />
+      <AppNav />
+      <h1 className="test">WorldWise</h1>
 
-      <h1>WorldWise</h1>
+      <Link to='/app'>Go to app</Link>
 
     </div>
   )
