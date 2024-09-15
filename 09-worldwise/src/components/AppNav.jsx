e2396907c82fb67465d11d133e16cf16e888fb1d
Quiatus <Quiatus@gmail.com>
2024-09-15 20:33:15 +0200

    add router

--- 09-worldwise/src/components/AppNav.jsx
+++ 09-worldwise/src/components/AppNav.jsx
@@ -1,5 +1,15 @@
 import styles from './AppNav.module.css'
+import { NavLink } from 'react-router-dom'
 
 export default function AppNav() {
-  return <nav className={styles.nav}>App navigation</nav>
+  return <nav className={styles.nav}>
+    <ul>
+      <li>
+        <NavLink to='cities'>Citites</NavLink>
+      </li>
+      <li>
+        <NavLink to='countries'>Countries</NavLink>
+      </li>
+    </ul>
+  </nav>
 }
