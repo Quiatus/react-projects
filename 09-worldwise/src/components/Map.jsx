01dc3a598e1818b5c18c23610968bb85b5d37471
Quiatus <Quiatus@gmail.com>
2024-09-16 23:18:58 +0200

    add router link

--- 09-worldwise/src/components/Map.jsx
+++ 09-worldwise/src/components/Map.jsx
@@ -1,9 +1,16 @@
+import { useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 
 export default function Map() {
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  const lat = searchParams.get('lat')
+  const lng = searchParams.get('lng')
+
   return (
     <div className={styles.mapContainer}>
-      MAP
+      <h1>MAP</h1>
+      <h2>Position: {lat}, {lng}</h2>
     </div>
   )
 }
