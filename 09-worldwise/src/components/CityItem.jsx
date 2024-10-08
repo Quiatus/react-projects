8f0eb6ec31b3a9280a64abfe2d9f1ffe68a53ff6
Quiatus <Quiatus@gmail.com>
2024-10-08 21:03:24 +0200

    add contextapi to city item

--- 09-worldwise/src/components/CityItem.jsx
+++ 09-worldwise/src/components/CityItem.jsx
@@ -1,15 +1,13 @@
 import { Link } from 'react-router-dom'
 
 import styles from './CityItem.module.css'
+import { useCities } from '../context/CitiesContext'
 
 export default function CityItem({ city }) {
+  const {currentCity} = useCities()
+  const { cityName, countryCode, date, id, position } = city
 
-  const { cityName, emoji, date, id, position } = city
-
-  const flagemojiToPNG = (flag) => {
-    var countryCode = Array.from(flag, (codeUnit) => codeUnit.codePointAt()).map(char => String.fromCharCode(char-127397).toLowerCase()).join('')
-    return (<img src={`https://flagcdn.com/24x18/${countryCode}.png`} alt='flag' />)
-  }
+  const flagemojiToPNG = (country) => <img src={`https://flagcdn.com/24x18/${country}.png`} alt='flag' />
 
   const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
@@ -20,8 +18,8 @@ export default function CityItem({ city }) {
 
   return (
     <li>
-      <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
-        <span className={styles.emoji}>{flagemojiToPNG(emoji)}</span>
+      <Link className={`${styles.cityItem} ${id === currentCity.id ? styles['cityItem--active'] : ""}`} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+        <span className={styles.emoji}>{flagemojiToPNG(countryCode)}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>{formatDate(date)}</time>
         <button className={styles.deleteBtn}>&times;</button>
