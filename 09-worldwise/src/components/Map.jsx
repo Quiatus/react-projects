f821b71b4d5a7f0e648195b6ede6da475ae32a2e
Quiatus <Quiatus@gmail.com>
2024-10-08 21:16:42 +0200

    add map api

--- 09-worldwise/src/components/Map.jsx
+++ 09-worldwise/src/components/Map.jsx
@@ -1,9 +1,12 @@
 import { useNavigate, useSearchParams } from 'react-router-dom'
+import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet"
+import { useState } from 'react'
 import styles from './Map.module.css'
 
 export default function Map() {
 
   const navigate = useNavigate()
+  const [mapPosition, setMapPosition] = useState([40, 0])
 
   const [searchParams, setSearchParams] = useSearchParams()
 
@@ -11,9 +14,18 @@ export default function Map() {
   const lng = searchParams.get('lng')
 
   return (
-    <div className={styles.mapContainer} onClick={() => {navigate('form')}}>
-      <h1>MAP</h1>
-      <h2>Position: {lat}, {lng}</h2>
+    <div className={styles.mapContainer}>
+      <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={mapPosition}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   )
 }
