470a894db13c478215c38e554455ef99c694c1b0
Quiatus <Quiatus@gmail.com>
2024-10-12 22:42:38 +0200

    add map coors

--- 09-worldwise/src/components/Map.jsx
+++ 09-worldwise/src/components/Map.jsx
@@ -1,22 +1,25 @@
-// import { useNavigate, useSearchParams } from 'react-router-dom'
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
-import { useState } from 'react'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from "react-leaflet"
+import { useEffect, useState } from 'react'
 import { useCities } from '../context/CitiesContext'
 import styles from './Map.module.css'
 
 export default function Map() {
-  // const navigate = useNavigate()
   const {cities} = useCities()
 
   const [mapPosition, setMapPosition] = useState([40, 0])
-  // const [searchParams, setSearchParams] = useSearchParams()
+  const [searchParams] = useSearchParams()
 
-  // const lat = searchParams.get('lat')
-  // const lng = searchParams.get('lng')
+  const lat = searchParams.get('lat')
+  const lng = searchParams.get('lng')
+
+  useEffect(() => {
+    if(lat && lng) setMapPosition([lat, lng])
+  }, [lat, lng])
 
   return (
     <div className={styles.mapContainer}>
-      <MapContainer className={styles.map} center={mapPosition} zoom={13} scrollWheelZoom={true}>
+      <MapContainer className={styles.map} center={mapPosition} zoom={8} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -27,7 +30,23 @@ export default function Map() {
           </Popup>
         </Marker>)
         }
+        <ChangeCenter position={mapPosition}/>
+        <DetectClick/>
       </MapContainer>
     </div>
   )
+}
+
+function ChangeCenter({ position }) {
+  const map = useMap()
+  map.setView(position)
+  return null
+}
+
+function DetectClick() {
+  const navigate = useNavigate()
+
+  useMapEvents({
+    click: e => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`)
+  })
 }
