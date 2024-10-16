3f84050d5f29595ceceb3bb0f912dbe7eea67d0a
Quiatus <Quiatus@gmail.com>
2024-10-16 18:57:29 +0200

    add login function

--- 09-worldwise/src/components/PageNav.jsx
+++ 09-worldwise/src/components/PageNav.jsx
@@ -1,20 +1,27 @@
-import { NavLink } from 'react-router-dom'
-import styles from "./PageNav.module.css"
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import styles from "./PageNav.module.css";
 
-export default function PageNav() {
+function PageNav() {
   return (
     <nav className={styles.nav}>
+      <Logo />
+
       <ul>
         <li>
-          <NavLink to='/'>Homepage</NavLink>
+          <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
-          <NavLink to='/pricing'>Pricing</NavLink>
+          <NavLink to="/product">Product</NavLink>
         </li>
         <li>
-          <NavLink to='/product'>Product</NavLink>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
-  )
+  );
 }
+
+export default PageNav;
--- 09-worldwise/src/components/Sidebar.jsx
+++ 09-worldwise/src/components/Sidebar.jsx
@@ -3,11 +3,13 @@ import Logo from './Logo'
 import AppNav from './AppNav'
 import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
+  // const navigate = useNavigate()
   return (
     <div className={styles.sidebar}>
-      <Logo />
+      <Link to={"/"}> <Logo /> </Link>
       <AppNav />
 
       <Outlet />
