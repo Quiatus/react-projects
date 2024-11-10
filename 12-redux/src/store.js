22c4fd231ec134245e98c0c06a7c184927ca49be
Quiatus <Quiatus@gmail.com>
2024-11-10 21:56:39 +0100

    complete store

--- 12-redux/src/store.js
+++ 12-redux/src/store.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux"
+import { combineReducers, createStore } from "redux"
 
 const initialState = {
   balance: 0,
@@ -6,7 +6,13 @@ const initialState = {
   loanPurpose: ""
 }
 
-function reducer(state = initialState, action) {
+const initialStateCustomer = {
+  fullName: "",
+  nationalID: "",
+  createdAt: ""
+}
+
+function accountReducer(state = initialState, action) {
   switch(action.type) {
     case "account/deposit":
       return {...state, balance: state.balance + action.payload}
@@ -31,24 +37,56 @@ function reducer(state = initialState, action) {
   }
 }
 
- const store = createStore(reducer)
+function customerReducer(state = initialStateCustomer, action) {
+  switch(action.type) {
+    case "customer/createCustomer":
+      return {
+        ...state, 
+        fullName: action.payload.fullName, 
+        nationalID: action.payload.nationalID, 
+        createdAt: action.payload.createdAt
+      }
+    case "customer/upadeName":
+      return {
+        ...state, 
+        fullName: action.payload
+      }
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: customerReducer
+})
+
+const store = createStore(rootReducer)
 
 //  store.dispatch({type: "account/deposit", payload: 500})
 //  store.dispatch({type: "account/requestLoan", payload: {amount: 200, purpose: "Coke"}})
-
 //  console.log(store.getState())
 
- function deposit(amount) {
+function deposit(amount) {
   return {type: "account/deposit", payload: amount}
- }
- function withdraw(amount) {
-  return {type: "account/withdraw", payload: amount}
- }
- function requestLoan(amount, purpose) {
-  return {type: "account/requestLoan", payload: { amount, purpose}}
- }
- function payLoan() {
-  return {type: "account/payLoan"}
- }
+}
 
- //store.dispatch(deposit(500))
+function withdraw(amount) {
+  return {type: "account/withdraw", payload: amount}
+}
+
+function requestLoan(amount, purpose) {
+  return {type: "account/requestLoan", payload: { amount, purpose}}
+}
+
+function payLoan() {
+  return {type: "account/payLoan"}
+}
+
+function createCustomer(fullName, nationalID) {
+  return {type: 'customer/createCustomer', payload: {fullName, nationalID, createdAt: new Date().toISOString()}}
+}
+
+function updateName(fullName) {
+  return {type: 'customer/upadeName', payload: fullName}
+}
