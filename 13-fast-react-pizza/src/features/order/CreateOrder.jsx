b826b05c5ee724c2f9e18a3be035602cd2c1f508
Quiatus <Quiatus@gmail.com>
2024-11-27 21:18:07 +0100

    style forms

--- 13-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 13-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -44,13 +44,13 @@ function CreateOrder() {
       <Form method="POST">
         <div>
           <label>First Name</label>
-          <input type="text" name="customer" required />
+          <input type="text" name="customer" required className="input"/>
         </div>
 
         <div>
           <label>Phone number</label>
           <div>
-            <input type="tel" name="phone" required />
+            <input type="tel" name="phone" required className="input"/>
           </div>
           {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
@@ -58,7 +58,7 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input type="text" name="address" required className="input"/>
           </div>
         </div>
 
@@ -67,6 +67,7 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
--- 13-fast-react-pizza/src/features/order/SearchOrder.jsx
+++ 13-fast-react-pizza/src/features/order/SearchOrder.jsx
@@ -14,7 +14,13 @@ export default function SearchOrder() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" placeholder="Search order #" value={query} onChange={e => setQuery(e.target.value)}/>
+      <input 
+        type="text" 
+        placeholder="Search order #" 
+        value={query} 
+        onChange={e => setQuery(e.target.value)} 
+        className="rounded-full px-4 py-2 text-sm bg-yellow-100 placeholder:text-stone-400 w-28 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:opacity-50"
+      />
     </form>
   )
 }
