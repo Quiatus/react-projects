9315ff19306abfb3246a7cb0026bb3d6dcfd0c07
Quiatus <Quiatus@gmail.com>
2024-12-01 15:42:54 +0100

    style menu

--- 13-fast-react-pizza/src/ui/Button.jsx
+++ 13-fast-react-pizza/src/ui/Button.jsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom"
 
-export default function Button({children, disabled, to}) {
+export default function Button({children, disabled, to, type}) {
+  
+  const base = "bg-yellow-400 uppercase font-semibold text-stone-800 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 disabled:cursor-not-allowedfocus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2"
 
-  const className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 sm:px-6 sm:py-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-300 disabled:cursor-not-allowedfocus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2"
+  const styles = {
+    primary: base + ' px-4 py-3 sm:px-6 sm:py-4',
+    small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs'
+  }
 
   if(to) {
-    return <Link to={to} className={className}>{children}</Link>
+    return <Link to={to} className={styles[type]}>{children}</Link>
   }
 
   return (
-    <button disabled={disabled} className={className}> {children}</button>
+    <button disabled={disabled} className={styles[type]}> {children}</button>
   )
 }
