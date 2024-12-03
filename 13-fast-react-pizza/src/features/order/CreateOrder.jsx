ef9d86006dfb4b2a7850d19befcc207e5a32de46
Quiatus <Quiatus@gmail.com>
2024-12-03 20:31:38 +0100

    implement user name state

--- 13-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 13-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) => /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
@@ -31,6 +32,7 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  const username = useSelector(state => state.user.username)
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
   const navigation = useNavigation()
@@ -45,7 +47,7 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input type="text" name="customer" required className="input grow"/>
+          <input type="text" name="customer" required className="input grow" defaultValue={username}/>
         </div>
 
         <div className="mb-5 flex gap-2 flex-col sm:flex-row sm:items-center">
--- 13-fast-react-pizza/src/ui/Home.jsx
+++ 13-fast-react-pizza/src/ui/Home.jsx
@@ -1,6 +1,9 @@
+import { useSelector } from 'react-redux';
 import CreateUser from '../features/user/CreateUser'
+import Button from './Button';
 
 function Home() {
+  const username = useSelector(state => state.user.username)
   return (
     <div className='my-10 sm:my-16 text-center px-4'>
       <h1 className="mb-8 text-center text-xl font-semibold md:text-3xl">
@@ -9,7 +12,7 @@ function Home() {
         <span className="text-yellow-500">Straight out of the oven, straight to you.</span>
       </h1>
 
-      <CreateUser />
+      {username === '' ? <CreateUser /> : <Button type='primary' to='/menu'>Continue ordering, {username}</Button>}
     </div>
   );
 }
