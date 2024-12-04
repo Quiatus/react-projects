5ffff9f54475162375b0f9386f7b4c3b2aa60a0e
Quiatus <Quiatus@gmail.com>
2024-12-04 20:02:16 +0100

    implement cart

--- 13-fast-react-pizza/src/features/cart/cartSlice.js
+++ 13-fast-react-pizza/src/features/cart/cartSlice.js
@@ -38,3 +38,5 @@ export default cartSlice.reducer
 
 export const getTotalCartQty = state => state.cart.cart.reduce((sum, item) => sum + item.qty, 0)
 export const getTotalCartPrice = state => state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0)
+
+export const getCart = state => state.cart.cart
