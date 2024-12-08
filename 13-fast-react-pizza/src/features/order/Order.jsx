a4216ac5ff93d5226db6d5c57373b437c21b8e49
Quiatus <Quiatus@gmail.com>
2024-12-08 14:59:17 +0100

    read data

--- 13-fast-react-pizza/src/features/order/Order.jsx
+++ 13-fast-react-pizza/src/features/order/Order.jsx
@@ -1,6 +1,6 @@
 // Test ID: IIDSAT
 
-import { useLoaderData } from "react-router-dom";
+import { useFetcher, useLoaderData } from "react-router-dom";
 import { getOrder } from "../../services/apiRestaurant";
 import {
   calcMinutesLeft,
@@ -9,9 +9,16 @@ import {
 } from "../../utils/helpers";
 
 import OrderItem from './OrderItem'
+import { useEffect } from "react";
 
 function Order() {
   const order = useLoaderData()
+
+  const fetcher = useFetcher()
+
+  useEffect(() => {
+    if(!fetcher.data && fetcher.state === 'idle') fetcher.load('/menu')
+  }, [fetcher])
 
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
@@ -47,7 +54,7 @@ function Order() {
       </div>
 
       <ul className="divide-y divide-stone-200 border-b border-t">
-        {cart.map(item => <OrderItem item={item} key={item.key}></OrderItem>)}
+        {cart.map(item => <OrderItem item={item} key={item.key} isLoadingIngredients={fetcher.state === 'loading'} ingredients={fetcher.data?.find(el => el.id === item.pizzaId).ingredients ?? []}></OrderItem>)}
       </ul>
 
       <div className="space-y-2 bg-stone-200 py-5 px-6">
