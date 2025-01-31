28bb9cbfc5aff7a4518d6ad81feabd84774cd6dc
Quiatus <Quiatus@gmail.com>
2025-01-31 15:22:39 +0100

    add delete functuion

--- 14-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 14-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { deleteCabin } from "../../services/apiCabins";
+
+import toast from 'react-hot-toast'
 
 const TableRow = styled.div`
   display: grid;
@@ -42,16 +46,28 @@ const Discount = styled.div`
 
 
 export default function CabinRow({cabin}) {
+  const {id: cabinId, name, maxCapacity, regularPrice, discount, image} = cabin
 
-  const {name, maxCapacity, regularPrice, discount, image} = cabin
+  const queryClient = useQueryClient()
+
+  const {isLoading: isDeleting, mutate } = useMutation({
+    mutationFn: deleteCabin,
+    onSuccess: () => {
+      toast.success("Cabin deleted")
+      queryClient.invalidateQueries({
+        queryKey: ["cabins"]
+      })
+    },
+    onError: (err) => toast.error(err.message)
+  })
   return (
     <TableRow role="row">
       <Img src={image}/>
       <Cabin>{name}</Cabin>
-      <div>Fits up ti {maxCapacity} guests</div>
+      <div>Fits up to {maxCapacity} guests</div>
       <Price>{formatCurrency(regularPrice)}</Price>
       <Discount>{formatCurrency(discount)}</Discount>
-      <button>Delete</button>
+      <button onClick={() => mutate(cabinId)} disabled={isDeleting}>Delete</button>
     </TableRow>
   )
 }
