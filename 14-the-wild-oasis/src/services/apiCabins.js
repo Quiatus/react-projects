5ca064a887e26a18735582161f9347c94f3b6ed9
Quiatus <Quiatus@gmail.com>
2025-02-02 11:03:34 +0100

    add upload picture

--- 14-the-wild-oasis/src/services/apiCabins.js
+++ 14-the-wild-oasis/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase"
+import supabase, { supabaseUrl } from "./supabase" 
 
 export async function getCabins() {
   const { data, error } = await supabase
@@ -14,15 +14,29 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll("/", "")
+
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`
   
   const { data, error } = await supabase
     .from('cabins')
-    .insert([newCabin])
+    .insert([{...newCabin, image: imagePath}])
     .select()
 
   if (error) {
     console.error(error)
     throw new Error('Cabins could not be created')
+  }
+
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image)
+
+  if(storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id)
+    console.error(storageError)
+    throw new Error('Cabins image could not be uploaded. Cabin could not be created')
   }
 
   return data
@@ -42,3 +56,4 @@ export async function deleteCabin(id) {
 
   return data
 }
+
--- 14-the-wild-oasis/src/services/supabase.js
+++ 14-the-wild-oasis/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://adcplxhzlytpbjycrxpo.supabase.co'
+export const supabaseUrl = 'https://adcplxhzlytpbjycrxpo.supabase.co'
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFkY3BseGh6bHl0cGJqeWNyeHBvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxNDExMzUsImV4cCI6MjA1MjcxNzEzNX0.hh_86hVHkpa5L5wE0GHz54Av8pm6poReuXsRffKekH4"
 const supabase = createClient(supabaseUrl, supabaseKey)
 
