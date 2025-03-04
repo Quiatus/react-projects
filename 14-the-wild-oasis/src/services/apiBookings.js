2c9ca8c5da19b66b4df4e18477a641b401b69fe2
Quiatus <Quiatus@gmail.com>
2025-03-04 11:05:18 +0100

    add sort to bookings

--- 14-the-wild-oasis/src/services/apiBookings.js
+++ 14-the-wild-oasis/src/services/apiBookings.js
@@ -4,7 +4,9 @@ import supabase from "./supabase";
 export async function getBookings({ filter, sortBy }) {
   let query = supabase.from("bookings").select('*, cabins(name), guests(fullName, email)')
 
-  if (filter !== null) query = query.eq(filter.field, filter.value) 
+  if (filter) query = query.eq(filter.field, filter.value) 
+
+  if (sortBy) query = query.order(sortBy.field, {ascending: sortBy.direction === "asc"})
 
   const {data, error} = await query
 
