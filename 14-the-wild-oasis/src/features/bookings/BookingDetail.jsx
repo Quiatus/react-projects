1a74a41869b938a796eb64c2a39586c91d944292
Quiatus <Quiatus@gmail.com>
2025-04-04 15:37:57 +0200

    add delete booking

--- 14-the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ 14-the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -14,6 +14,9 @@ import { useBooking } from "./useBooking";
 import { useNavigate } from "react-router-dom";
 import { HiArrowUpOnSquare } from "react-icons/hi2";
 import { useCheckout } from "../check-in-out/useCheckout";
+import Modal from "../../ui/Modal"
+import ConfirmDelete from "../../ui/ConfirmDelete"
+import { useDeleteBooking } from "./useDeleteBooking";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -24,10 +27,11 @@ const HeadingGroup = styled.div`
 function BookingDetail() {
   const {booking, isLoading} = useBooking()
   const {checkout, isCheckingOut} = useCheckout()
+  const { deleteBooking, isDeleting } = useDeleteBooking()
   const moveBack = useMoveBack();
   const navigate = useNavigate()
 
-  if(isLoading || isCheckingOut) return <Spinner />
+  if(isLoading || isCheckingOut || isDeleting) return <Spinner />
 
   const {status, id: bookingId} = booking
 
@@ -58,6 +62,16 @@ function BookingDetail() {
         {status === "checked-in" && <Button icon={<HiArrowUpOnSquare />} onClick={() => checkout(bookingId)} disabled={isCheckingOut}>
           Check out
         </Button>}
+
+        <Modal>
+          <Modal.Open opens='delete'>
+            <Button variation='danger'>Delete booking</Button>
+          </Modal.Open>
+
+          <Modal.Window name='delete'>
+            <ConfirmDelete resourceName='booking' onConfirm={() => deleteBooking(bookingId, {onSettled: () => navigate(-1)})} disabled={isDeleting}/>
+          </Modal.Window>
+        </Modal>
 
         <Button variation="secondary" onClick={moveBack}>
           Back
